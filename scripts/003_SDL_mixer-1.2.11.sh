775bb834185ea93ca963a33bf861a6c3abc28b2c
gameblabla <gameblabla@openmailbox.org>
2020-04-20 21:22:23 +0200

    Fix SDL_mixer.

--- scripts/003_SDL_mixer-1.2.11.sh
+++ scripts/003_SDL_mixer-1.2.11.sh
@@ -8,16 +8,4 @@ cp ../config.sub ../config.guess build-scripts/ || { exit 1; }
 
 cat ../patches/SDL_mixer-1.2.11.patch | patch -p1 || { exit 1; }
 
-LIBMIKMOD_CONFIG="$PS3DEV/portlibs/ppu/bin/libmikmod-config"
-export LIBMIKMOD_CONFIG
-./configure --prefix="$PS3DEV/portlibs/ppu" --host=powerpc64-ps3-elf \
-	--disable-sdltest \
-	--with-sdl-exec-prefix="$PS3DEV/portlibs/ppu" \
-	--disable-shared \
-	--disable-music-cmd \
-	--disable-music-ogg-shared \
-	--disable-music-mp3 \
-	--disable-music-flac \
-    	|| { exit 1; }
-
 make && make install || { exit 1; }
